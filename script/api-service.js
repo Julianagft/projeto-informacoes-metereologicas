38886abb0a125343c2f2b4ed74bbb537aa6b18ee
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2023-12-21 16:02:49 -0300

    tem jeito não

--- script/api-service.js
+++ script/api-service.js
@@ -1,38 +1,11 @@
 // COMUNICAÇÃO COM A API
-const parseString = require('xml2js').parseString;
+import fetch from 'node-fetch';
+import { parseString } from 'xml2js';
 
 const urlTempo = `http://servicos.cptec.inpe.br/XML/cidade/?id=${encodeURIComponent(cidadeId)}/previsao.xml`;
 
 const urlLocalidade = `http://servicos.cptec.inpe.br/XML/listaCidades?city=${encodeURIComponent(cidade.nome)}`;
 
-const buscaLocalidade = async () => {
-    try {
-        const resposta = await fetch(urlLocalidade);
-        if (resposta.ok) {
-            const data = await resposta.json();
-            return data
-        } else {
-            console.error('Erro na requisição: ',erro);
-        }
-    } catch(erro) {
-        window.alert('Erro na requisição. Tente novamente em alguns minutos!');
-        console.error('Erro na requisição: ', erro)
-    }
-}
-
-const previsaoLocalidade = async () => {
-    try {
-        const resposta = await fetch(urlTempo);
-        if (resposta.ok) {
-            const data = await resposta.json();
-            return data;
-        } else {
-            console.error('Erro ao obter dados:', resposta.statusText);
-        }
-    } catch (erro) {
-        console.error('Erro na requisição: ', erro)
-     }
-}
 // Função para converter XML em objeto JavaScript
 const parseXml = (xml) => {
     return new Promise((resolve, reject) => {
@@ -46,6 +19,42 @@ const parseXml = (xml) => {
     });
   };
 
+
+const buscaLocalidade = async (municipio) => {
+    try {
+        const resposta = await fetch(urlLocalidade);
+        console.log('Resposta da requisição:', resposta);
+        if (resposta.ok) {
+            const data = await resposta.text();
+            const resultado = await parseXml(data);
+            console.log('Resultado da busca de localidade:', JSON.stringify(resultado, null, 2));
+            console.log('Resultado da previsão de localidade:', JSON.stringify(resultado, null, 2));
+            return resultado.cidades.cidade;
+        } else {
+            console.error('Erro na requisição: ',resposta.statusText);
+            return null
+        }
+    } catch(erro) {
+        console.error('Erro na requisição: ', erro)
+        return null;
+    }
+}
+
+const previsaoLocalidade = async (cidadeId) => {
+    try {
+        const resposta = await fetch(urlTempo);
+        if (resposta.ok) {
+            const data = await resposta.text();
+            const resultado = await parseXml(data)
+            return resultado;
+        } else {
+            console.error('Erro ao obter dados:', resposta.statusText);
+        }
+    } catch (erro) {
+        console.error('Erro na requisição: ', erro);
+        return null;
+     }
+}
 
 export const service = {
     previsaoLocalidade,
